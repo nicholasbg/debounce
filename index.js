303ecc6f6e94c742405e986a9fb372587f576f57
nicholasbg <nicholasbg@gmail.com>
2022-06-19 10:14:09 -0500

    update for npm

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
-export default (() => {
+/**
+ * @description Debounce
+ * 
+ * @param {function} callback function to debounce
+ * @param {string | number | symbol} id unique id required to avoid collisions
+ * when using independent instances of the function
+ * @param {number} delay time, in milliseconds that the timer should wait before
+ * the specified function or code is executed
+ * 
+ * @return {undefined}
+ * 
+ * @example
+ * debounce(function(){
+ *   console.log('bababoo');
+ * }, 'unique-id-0', 100);
+ */
+module.exports = (() => {
   const timeouts = {};
   return (callback, id, delay) => {
     timeouts[id] && clearTimeout(timeouts[id]);
