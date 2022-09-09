4dacac3c64699b57530b0b69ab686640a42fd552
Nick Burke-Gaffney <nicholasbg@gmail.com>
2022-09-09 11:41:48 -0500

    && to ??

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 /**
  * @description Debounce
- * 
+ *
  * @param {function} callback function to debounce
  * @param {string | number | symbol} id unique id required to avoid collisions
  * when using independent instances of the function
  * @param {number} delay time, in milliseconds that the timer should wait before
  * the specified function or code is executed
- * 
+ *
  * @return {undefined}
- * 
+ *
  * @example
  * debounce(function(){
  *   console.log('bababoo');
@@ -17,7 +17,7 @@
 module.exports = (() => {
   const timeouts = {};
   return (callback, id, delay) => {
-    timeouts[id] && clearTimeout(timeouts[id]);
+    timeouts[id] ?? clearTimeout(timeouts[id]);
     timeouts[id] = setTimeout(callback, delay);
   };
 })();
